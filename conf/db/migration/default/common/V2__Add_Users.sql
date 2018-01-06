5d627ce9df0573e04869948be3fb6681b3f91820
tofu511 <mokmok511@gmail.com>
2018-01-06 15:19:41 +0900

    add DB setting

---
+++ conf/db/migration/default/common/V2__Add_Users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test1', 'test1@test.com', '$2a$10$n8Sz8lQ3ptFg0wZM//SgKeobwwWYuvbecEt2QCJiWnHrAi8LoeXOm', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test2', 'test2@test.com', '$2a$10$n8Sz8lQ3ptFg0wZM//SgKeobwwWYuvbecEt2QCJiWnHrAi8LoeXOm', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test3', 'test3@test.com', '$2a$10$n8Sz8lQ3ptFg0wZM//SgKeobwwWYuvbecEt2QCJiWnHrAi8LoeXOm', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test4', 'test4@test.com', '$2a$10$n8Sz8lQ3ptFg0wZM//SgKeobwwWYuvbecEt2QCJiWnHrAi8LoeXOm', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test5', 'test5@test.com', '$2a$10$n8Sz8lQ3ptFg0wZM//SgKeobwwWYuvbecEt2QCJiWnHrAi8LoeXOm', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test6', 'test6@test.com', '$2a$10$n8Sz8lQ3ptFg0wZM//SgKeobwwWYuvbecEt2QCJiWnHrAi8LoeXOm', now(), now());
