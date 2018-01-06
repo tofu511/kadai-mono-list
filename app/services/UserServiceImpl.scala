ad60dcba2ce48da1f7b3b7605f4a19c960d62c88
tofu511 <mokmok511@gmail.com>
2018-01-06 17:00:31 +0900

    implement user registration

---
+++ app/services/UserServiceImpl.scala
@@ -0,0 +1,23 @@
+package services
+
+import javax.inject._
+import models.User
+import scalikejdbc.DBSession
+
+import scala.util.Try
+
+@Singleton
+class UserServiceImpl extends UserService {
+
+  override def create(user: User)(implicit dBSession: DBSession): Try[Long] = Try {
+    User.create(user)
+  }
+
+  override def findById(userId: Long)(implicit dBSession: DBSession): Try[Option[User]] = Try {
+    User.findById(userId)
+  }
+
+  override def findByEmail(email: String)(implicit dBSession: DBSession): Try[Option[User]] = Try {
+    User.where('email -> email).apply().headOption
+  }
+}
